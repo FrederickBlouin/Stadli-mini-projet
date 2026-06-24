4e01b2f3337fe252b13e94d2ba36b6de0966bdfe
FrederickBlouin <frederick.blouin.5@gmail.com>
2026-06-24 18:38:25 -0400

    feat: Ajout des routes d'authentifications

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,6 +2,12 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+// Importe les routes
+import authRoutes from "./routes/auth";
+
+// Utilisation des routes en tant que middleware
+app.route("/auth", authRoutes);
+
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })

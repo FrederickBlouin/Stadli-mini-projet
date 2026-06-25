f37c25f294f38579a17ef8bdd8b8c3021347b324
FrederickBlouin <frederick.blouin.5@gmail.com>
2026-06-25 16:12:49 -0400

    feat: ajout de la fonction createEvenement dans evenementController

--- api/src/controllers/evenementController.ts
+++ api/src/controllers/evenementController.ts
@@ -74,3 +74,56 @@ export const getEvenement = async (context: Context) => {
     );
   }
 };
+
+export const createEvenement = async (context: Context) => {
+  try {
+    const body = await context.req.json();
+
+    const { titre, description, date } = body;
+
+    if (!titre || !description || !date) {
+      return context.json(
+        formatErrorResponse(
+          400,
+          "Bad Request",
+          "Un des paramètres est manquant",
+          context.req.path
+        ),
+        400
+      );
+    }
+
+    const result = await context.env.DB.prepare(
+      `INSERT INTO evenements 
+        (titre, description, date)
+       VALUES (?, ?, ?)`
+    )
+      .bind(titre, description, date)
+      .run();
+
+    return context.json(
+      formatSuccessResponse(
+        201,
+        "Événement créé avec succès",
+        {
+          id: result.meta.last_row_id,
+          titre,
+          description,
+          date,
+        },
+        context.req.path
+      ),
+      201
+    );
+  } catch (err) {
+    return context.json(
+      formatErrorResponse(
+        500,
+        "Internal Server Error",
+        "Erreur serveur",
+        context.req.path
+      ),
+      500
+    );
+  }
+};
